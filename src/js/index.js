327d3c963975fc29ffb601502539de58ae4693a1
Andrew Susanto <73069927+AndrewJSusanto@users.noreply.github.com>
2025-03-21 17:07:43 -0700

    Image sizing, overhaul of main page, adjustments to singers page, styling adjustments, interim push and stash

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,19 @@
-import { addDotBtnsAndClickHandlers } from './buttons.js';
+// import { addDotBtnsAndClickHandlers } from './buttons.js';
+import { addPrevNextBtnsClickHandlers } from './buttons.js';
 
 const emblaNode = document.querySelector('.embla');
-const options = { loop: false };
+const options = { loop: true };
 const autoplayOptions = { delay: 10000, stoponInteraction: true};
 const plugins = [EmblaCarouselAutoplay(autoplayOptions)];
 
 const rootNode = document.querySelector('.embla');
 const viewportNode = rootNode.querySelector('.embla__viewport');
 
-// const buttonContainer = document.getElementById('button-container');
-// const prevButtonNode = rootNode.querySelector('.embla__prev')
-// const nextButtonNode = rootNode.querySelector('.embla__next')
+const buttonContainer = document.getElementById('button-container');
+const prevButtonNode = rootNode.querySelector('.embla__button--prev')
+const nextButtonNode = rootNode.querySelector('.embla__button--next')
 
-const dotsNode = rootNode.querySelector('.embla__dots');
+// const dotsNode = rootNode.querySelector('.embla__dots');
 const emblaApi = EmblaCarousel(viewportNode, options, plugins);
 
 const onNavButtonClick = (emblaApi) => {
@@ -27,13 +28,17 @@ const onNavButtonClick = (emblaApi) => {
   resetOrStop()
 }
 
-const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(
-    emblaApi,
-    dotsNode,
-    onNavButtonClick
-)
+// const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(
+//     emblaApi,
+//     dotsNode,
+//     onNavButtonClick
+// )
 
-emblaApi.on('destroy', removeDotBtnsAndClickHandlers)
+const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(emblaApi, prevButtonNode, nextButtonNode)
+
+
+// emblaApi.on('destroy', removeDotBtnsAndClickHandlers)
+emblaApi.on('destroy', removePrevNextBtnsClickHandlers)
 
 // prevButtonNode.addEventListener('click', embla.scrollPrev, false);
 // nextButtonNode.addEventListener('click', embla.scrollNext, false);

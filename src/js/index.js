2a77478867878ad8480c3283afcdbd0cfba41ca2
Andrew Susanto <73069927+AndrewJSusanto@users.noreply.github.com>
2025-04-11 12:59:35 -0700

    Website overhaul test branch
    Listed changes in plans.txt

--- src/js/index.js
+++ src/js/index.js
@@ -42,3 +42,39 @@ emblaApi.on('destroy', removePrevNextBtnsClickHandlers)
 
 // prevButtonNode.addEventListener('click', embla.scrollPrev, false);
 // nextButtonNode.addEventListener('click', embla.scrollNext, false);
+
+
+// Autoplay onClick event to pause/play background video (click anywhere on landing)
+var bgv = document.getElementById("IWLME");
+var landing = document.getElementById("landing");
+landing.addEventListener("click", function(e) {
+    if (bgv.paused) {
+      bgv.play();
+      bgv.style.filter = "none";
+    }
+    else {
+      bgv.pause();
+      bgv.style.filter = "blur(5px) brightness(0.4)";
+      bgv.style.transition = "all 0.6s ease-in-out";
+    }
+})
+
+// Prevents autoplay onclick event from triggering when clicking the link redirect
+var concertRedirect = document.getElementById("concert-redirect");
+concertRedirect.addEventListener("click", function(e) {
+  e.stopPropagation();
+})
+
+
+
+// NavBar mobile interaction (injected instead)
+
+let navbar = document.getElementById("navbar-mobile");
+let popout = document.getElementById("navbar-mobile-pop-up");
+let overlay = document.getElementById("overlay");
+navbar.addEventListener("click", function(e) {
+  navbar.classList.toggle("change");
+  popout.classList.toggle("change");
+  overlay.classList.toggle("change");
+  console.log(popout);
+})
